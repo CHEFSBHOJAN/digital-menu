09c2a5a0477fe28dd4b30b707aea9268762f1c3e
nagesh-merva <officialnagesh.merva@gmail.com>
2024-08-06 00:09:36 +0530

    updates

--- frontend/src/components/MyComponent.jsx
+++ frontend/src/components/MyComponent.jsx
@@ -12,10 +12,10 @@ function MyComponent({ outlet }) {
   useEffect(() => {
     let url
     if (outlet === "Margao") {
-      url = "https://digital-menu-nluo.vercel.app/api/getmenumargao"
+      url = "https://digital-menu-6maa.onrender.com/api/getmenumargao"
     }
     else {
-      url = "https://digital-menu-nluo.vercel.app/api/getmenuponda"
+      url = "https://digital-menu-6maa.onrender.com/api/getmenuponda"
     }
     fetch(url)
       .then((response) => response.json())
