34e47ca9db100ad1c48c8596b9f35aa781a532f7
nagesh-merva <officialnagesh.merva@gmail.com>
2024-08-07 20:21:44 +0530

    modified

--- frontend/src/components/ChefsBhojan.jsx
+++ frontend/src/components/ChefsBhojan.jsx
@@ -5,12 +5,12 @@ import MenuButton from "./MenuButton"
 function ChefsBhojan({ outlet }) {
 
   return (
-    <main className="flex overflow-hidden flex-col items-center px-14 pt-56 pb-32 mx-auto w-full tracking-tight bg-yellow-400 max-w-[480px]">
+    <main className="flex overflow-hidden flex-col justify-center items-center px-14 pt-56 pb-32 mx-auto w-full h-screen tracking-tight bg-yellow-400 max-w-[480px]">
       <img
         loading="lazy"
         src="/logo.png"
         alt="Chefs Bhojan logo"
-        className="object-contain self-stretch w-full aspect-[1.2]"
+        className="object-contain w-1/3 aspect-[1.2] "
       />
       <WelcomeMessage outlet={outlet} />
       <MenuButton outlet={outlet} />
--- frontend/src/components/WelcomeMessage.jsx
+++ frontend/src/components/WelcomeMessage.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 function WelcomeMessage({ outlet }) {
   return (
-    <section className="mt-36 text-lg text-center text-black">
+    <section className="mt-14 text-lg text-center text-black">
       Welcome to Chefs Bhojan <br />
       <span className="font-bold text-red-900">{outlet}, Goa</span>
     </section>
